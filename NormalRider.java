ecff7d8deca61daf643ec3146cffb7b63f1daed4
RDG209 <rgaeta209@gmail.com>
2024-10-06 10:45:45 -0700

    Add files via upload

---
+++ NormalRider.java
@@ -0,0 +1,27 @@
+
+public class NormalRider extends Rider implements TicketPrice {
+  static double totalGate;
+  private double ticketPrice;
+
+  public NormalRider() {
+    
+  }
+  public NormalRider(int time) {
+     super(time);
+    setTicketPrice();
+    setTotalGate();
+  }
+
+  public double getTotalGate() {
+    return totalGate;
+  }
+  public void setTotalGate() {
+    totalGate += ticketPrice;
+  }
+  public double getTicketPrice() {
+    return this.ticketPrice;
+  }
+  public void setTicketPrice() {
+    this.ticketPrice = BASEPRICE;
+  }
+}
